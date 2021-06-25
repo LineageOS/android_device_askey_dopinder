b459fd9eaeea23c12e23323e25c852b402019d29
Aaron Kling <webgeek1234@gmail.com>
2021-06-25 01:04:25 -0400

    dopinder: Build Realtek 8822cs module inline
    Change-Id: I042931d6361420868f53f682c70fee8c5a2b589d

--- device.mk
+++ device.mk
@@ -9,3 +9,7 @@ DEVICE_PATH := device/askey/dopinder
 $(call inherit-product, vendor/askey/dopinder/dopinder-vendor.mk)
 
 $(call inherit-product, device/amlogic/g12-common/g12.mk)
+
+## Kernel Modules
+PRODUCT_PACKAGES += \
+    8822cs
