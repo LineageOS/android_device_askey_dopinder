0c337c100b39dc3e86d2689ed941265d872bbe6e
Nolen Johnson <johnsonnolen@gmail.com>
2023-12-11 01:21:39 -0500

    dopinder: Disable systemcontrol_static in recovery
    Change-Id: Ia5943a6c68963add1ab41f632d522a15b7108879

--- device.mk
+++ device.mk
@@ -18,7 +18,8 @@ endif
 
 ## Init-Files
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
+    $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc \
+    $(LOCAL_PATH)/init-files/init.recovery.dopinder.rc:recovery/root/init.recovery.dopinder.rc
 
 ## Kernel Modules
 PRODUCT_PACKAGES += \
