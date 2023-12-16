0b21f38eccd24e88a1867991c6aeaa4aac9b2b47
Bruno Martins <bgcngm@gmail.com>
2023-12-16 21:54:10 +0000

    dopinder: Properly build WLAN kernel module
    Change-Id: I1b02fd710c43ef66025d5d395728785c0ce3ddbe

--- device.mk
+++ device.mk
@@ -22,10 +22,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.target.rc \
     $(LOCAL_PATH)/init-files/init.recovery.dopinder.rc:recovery/root/init.recovery.dopinder.rc
 
-## Kernel Modules
-PRODUCT_PACKAGES += \
-    8822cs
-
 ## Keylayout (IR)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl
