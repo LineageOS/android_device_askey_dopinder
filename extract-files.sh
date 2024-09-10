2b944663e13a98f936f6757cf336c425170ae1b8
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    dopinder: Shim libvendorfont with libcrypto_shim_system_ext
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,10 @@
 
 function blob_fixup() {
     case "${1}" in
+        system_ext/lib/libvendorfont.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libcrypto_shim_system_ext.so" "${2}"
+            ;;
         vendor/bin/hw/android.hardware.security.keymint-service.amlogic)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V3-ndk.so" "${2}"
