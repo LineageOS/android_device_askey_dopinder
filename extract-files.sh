852e9dbc39015d8913023b9adcbf6ed3bcda2ae6
Bruno Martins <bgcngm@gmail.com>
2024-10-06 10:32:13 +0100

    dopinder: Patch keymint service to replace ndk_platform backend
    At the same time bump the ndk version where applicable to fix:
    
    error: module "android.hardware.security.keymint-service.amlogic" variant
    "android_vendor_arm_armv8-a": depends on multiple versions of the same aidl_interface:
    android.hardware.security.keymint-V1-ndk-source,
    android.hardware.security.keymint-V3-ndk-source
    
    Change-Id: I394545d7af4422cae2c446278a681cbbc353c9f3

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,12 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/bin/hw/android.hardware.security.keymint-service.amlogic)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V3-ndk.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.hardware.security.secureclock-V1-ndk_platform.so" "android.hardware.security.secureclock-V1-ndk.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "${2}"
+            ;;
         vendor/etc/init/tee-supplicant.rc)
             [ "$2" = "" ] && return 0
             sed -i 's#/vendor/lib/#/vendor/lib/modules/#g' "${2}"
