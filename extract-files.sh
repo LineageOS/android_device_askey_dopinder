e2b6f93f62d60e271af8a23a9a626e14ccade97e
Nolen Johnson <johnsonnolen@gmail.com>
2024-10-09 17:34:29 -0400

    dopinder: Decommonize TEE
    Change-Id: Ic2dffaa76ab5f5435fac781975ef17ebf114d4ee

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/etc/init/tee-supplicant.rc)
+            [ "$2" = "" ] && return 0
+            sed -i 's#/vendor/lib/#/vendor/lib/modules/#g' "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
