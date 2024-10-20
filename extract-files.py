3e4efebd430cc2e97e60c742de3ffd2b95ca1903
Bruno Martins <bgcngm@gmail.com>
2024-10-20 11:35:53 +0100

    dopinder: Adopt Python extract utils
    Change-Id: I56cc92329e15a50a86da2239b91822aee59e8d2b

---
+++ extract-files.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/amlogic/g12-common',
+]
+
+def lib_fixup_system_ext_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'system_ext' else None
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'libamavutils',
+    ): lib_fixup_system_ext_suffix,
+    (
+        'vendor.amlogic.hardware.subtitleserver@1.0',
+        'libvendorfont',
+        'libsubtitlebinder',
+    ): lib_fixup_vendor_suffix,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib/libvendorfont.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+    'vendor/bin/hw/android.hardware.security.keymint-service.amlogic': blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V3-ndk.so')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so'),
+    'vendor/etc/init/tee-supplicant.rc': blob_fixup()
+        .regex_replace('/vendor/lib/', '/vendor/lib/modules/')
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'dopinder',
+    'askey',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, '../amlogic/g12-common', module.vendor)
+    utils.run()
