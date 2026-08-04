18ebe638e83c3ded0d7c82f2891ec4e1bb5359d6
Nolen Johnson <johnsonnolen@gmail.com>
2026-08-03 21:18:53 -0400

    dopinder: Swap to undated SPDX identifiers everywhere
    Change-Id: I6dd90d5471e91cfcbec436a4811f2b8f690afb67

---
+++ bluetooth/include/bdroid_buildcfg.h
@@ -0,0 +1,19 @@
+/*
+* SPDX-FileCopyrightText: The LineageOS Project
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BLE_VND_INCLUDED TRUE
+
+#define BTM_BLE_CONN_INT_MIN_DEF     0x18
+#define BTM_BLE_CONN_INT_MAX_DEF     0x28
+#define BTM_BLE_CONN_TIMEOUT_DEF     200
+/* minimum acceptable connection interval */
+#define BTM_BLE_CONN_INT_MIN_LIMIT 0x0006  /*7.5ms=6*1.25*/
+
+#define BTM_BLE_SCAN_SLOW_INT_1 512
+
+#endif
